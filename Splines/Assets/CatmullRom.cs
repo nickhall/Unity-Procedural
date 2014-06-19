ebd2a699aba7794264008f17149f97881545f38c
Nick Hall <iamssb@gmail.com>
2014-06-19 12:26:30 +0900

    Got those Catmull-Rom splines working
    Also I'm an idiot

--- Splines/Assets/CatmullRom.cs
+++ Splines/Assets/CatmullRom.cs
@@ -21,10 +21,15 @@ public class CatmullRom : MonoBehaviour
         Vector3 m0;
         Vector3 m1;
 
-        CurveCoordinates = new Vector3[CurveResolution * (Points.Length - 1) + 1];
+        CurveCoordinates = new Vector3[(CurveResolution) * (Points.Length - 1)];
 
         for (int i = 0; i < Points.Length - 1; i++)
         {
+            if (Points[i] == null || Points[i + 1] == null || (i > 0 && Points[i - 1] == null) || (i < Points.Length - 2 && Points[i + 2] == null))
+            {
+                return;
+            }
+
             p0 = Points[i].transform.position;
             p1 = Points[i + 1].transform.position;
             if (i > 0)
@@ -48,14 +53,26 @@ public class CatmullRom : MonoBehaviour
             float t;
             float pointStep = 1.0f / CurveResolution;
 
+            if (i == Points.Length - 2)
+            {
+                pointStep = 1.0f / (CurveResolution - 1);
+                // last point of last segment should reach p1
+            }  
+
             for (int j = 0; j < CurveResolution; j++)
             {
+                // Catmull-Rom splines are Hermite curves with special tangent values.
+                // Hermite curve formula:
+                // (2t^3 - 3t^2 + 1) * p0 + (t^3 - 2t^2 + t) * m0 + (-2t^3 + 3t^2) * p1 + (t^3 - t^2) * m1
+                // For points p0 and p1 passing through points m0 and m1 interpolated over t = [0, 1]
+                // Tangent M[k] = (P[k+1] - P[k-1]) / 2
+                // With [] indicating subscript
                 t = j * pointStep;
                 position = (2.0f * t * t * t - 3.0f * t * t + 1.0f) * p0
                     + (t * t * t - 2.0f * t * t + t) * m0
                     + (-2.0f * t * t * t + 3.0f * t * t) * p1
                     + (t * t * t - t * t) * m1;
-                CurveCoordinates[j + i * Points.Length] = position;
+                CurveCoordinates[i * CurveResolution + j] = position;
             }
         }
 
