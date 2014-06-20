6806ded81c93f8eb2b24b3ffeea7f98f3a0453e1
Nick Hall <iamssb@gmail.com>
2014-06-20 16:14:40 +0900

    CR spline looping

--- Splines/Assets/CatmullRom.cs
+++ Splines/Assets/CatmullRom.cs
@@ -10,11 +10,42 @@ public class CatmullRom : MonoBehaviour
     public Vector3[] CurveCoordinates;
     public Vector3[] Tangents;
     public bool ClosedLoop = false;
+    public struct CatmullRomSpline
+    {
+        //
+    }
 
 	void Start()
     {
 	
 	}
+
+    public static Vector3 Interpolate(Vector3 start, Vector3 end, Vector3 tanPoint1, Vector3 tanPoint2, float t)
+    {
+        // Catmull-Rom splines are Hermite curves with special tangent values.
+        // Hermite curve formula:
+        // (2t^3 - 3t^2 + 1) * p0 + (t^3 - 2t^2 + t) * m0 + (-2t^3 + 3t^2) * p1 + (t^3 - t^2) * m1
+        // For points p0 and p1 passing through points m0 and m1 interpolated over t = [0, 1]
+        // Tangent M[k] = (P[k+1] - P[k-1]) / 2
+        // With [] indicating subscript
+        Vector3 position = (2.0f * t * t * t - 3.0f * t * t + 1.0f) * start
+            + (t * t * t - 2.0f * t * t + t) * tanPoint1
+            + (-2.0f * t * t * t + 3.0f * t * t) * end
+            + (t * t * t - t * t) * tanPoint2;
+
+        return position;
+    }
+
+    public static Vector3 Interpolate(Vector3 start, Vector3 end, Vector3 tanPoint1, Vector3 tanPoint2, float t, out Vector3 tangent)
+    {
+        // Calculate tangents
+        // p'(t) = (6t² - 6t)p0 + (3t² - 4t + 1)m0 + (-6t² + 6t)p1 + (3t² - 2t)m1
+        tangent = (6 * t * t - 6 * t) * start
+            + (3 * t * t - 4 * t + 1) * tanPoint1
+            + (-6 * t * t + 6 * t) * end
+            + (3 * t * t - 2 * t) * tanPoint2;
+        return Interpolate(start, end, tanPoint1, tanPoint2, t);
+    }
 	
 	void Update()
     {
@@ -36,64 +67,86 @@ public class CatmullRom : MonoBehaviour
         CurveCoordinates = new Vector3[pointsToMake];
         Tangents = new Vector3[pointsToMake];
 
-        for (int i = 0; i < Points.Length - 1; i++)
+        int closedAdjustment = ClosedLoop ? 0 : 1;
+
+        // First for loop goes through each individual control point and connects it to the next, so 0-1, 1-2, 2-3 and so on
+        for (int i = 0; i < Points.Length - closedAdjustment; i++)
         {
-            if (Points[i] == null || Points[i + 1] == null || (i > 0 && Points[i - 1] == null) || (i < Points.Length - 2 && Points[i + 2] == null))
-            {
-                return;
-            }
+            //if (Points[i] == null || Points[i + 1] == null || (i > 0 && Points[i - 1] == null) || (i < Points.Length - 2 && Points[i + 2] == null))
+            //{
+            //    return;
+            //}
 
             p0 = Points[i].transform.position;
-            p1 = Points[i + 1].transform.position;
-            if (i > 0)
+            p1 = (ClosedLoop == true && i == Points.Length - 1) ? Points[0].transform.position : Points[i + 1].transform.position;
+
+            // Tangent calculation for each control point
+            // Tangent M[k] = (P[k+1] - P[k-1]) / 2
+            // With [] indicating subscript
+
+            // m0
+            if (i == 0)
+            {
+                m0 = ClosedLoop ? 0.5f * (p1 - Points[Points.Length - 1].transform.position) : p1 - p0;
+            }
+            else
             {
                 m0 = 0.5f * (p1 - Points[i - 1].transform.position);
             }
+
+            // m1
+            if (ClosedLoop)
+            {
+                if (i == Points.Length - 1)
+                {
+                    m1 = 0.5f * (Points[(i + 2) % Points.Length].transform.position - p0);
+                }
+                else if (i == 0)
+                {
+                    m1 = 0.5f * (Points[i + 2].transform.position - p0);
+                }
+                else
+                {
+                    m1 = 0.5f * (Points[(i + 2) % Points.Length].transform.position - p0);
+                }
+            }
             else
             {
-                m0 = p1 - p0;
-                //m0 = 0.5f * (p1 - Points[Points.Length - 1].transform.position);
+                if (i < Points.Length - 2)
+                {
+                    m1 = 0.5f * (Points[(i + 2) % Points.Length].transform.position - p0);
+                }
+                else
+                {
+                    m1 = p1 - p0;
+                }
             }
-            if (i < Points.Length - 2)
-            {
-                m1 = 0.5f * (Points[i + 2].transform.position - p0);
-            }
-            else
-            {
-                m1 = p1 - p0;
-            }
+
+            //if (i >= Points.Length - 2)
+            //{
+            //    m1 = ClosedLoop ? 0.5f * (p1 - Points[Points.Length - 1].transform.position) : p1 - p0;
+            //}
+            //else
+            //{
+            //    m1 = 0.5f * (Points[(i + 2) % Points.Length].transform.position - p0);
+            //}
 
             Vector3 position;
             float t;
             float pointStep = 1.0f / CurveResolution;
 
-            if (i == Points.Length - 2)
+            if ((i == Points.Length - 2 && ClosedLoop == false) || (i == Points.Length - 1 && ClosedLoop))
             {
                 pointStep = 1.0f / (CurveResolution - 1);
                 // last point of last segment should reach p1
-            }  
-
+            }
+            // Second for loop actually creates the spline for this particular segment
             for (int j = 0; j < CurveResolution; j++)
             {
-                // Catmull-Rom splines are Hermite curves with special tangent values.
-                // Hermite curve formula:
-                // (2t^3 - 3t^2 + 1) * p0 + (t^3 - 2t^2 + t) * m0 + (-2t^3 + 3t^2) * p1 + (t^3 - t^2) * m1
-                // For points p0 and p1 passing through points m0 and m1 interpolated over t = [0, 1]
-                // Tangent M[k] = (P[k+1] - P[k-1]) / 2
-                // With [] indicating subscript
                 t = j * pointStep;
-                position = (2.0f * t * t * t - 3.0f * t * t + 1.0f) * p0
-                    + (t * t * t - 2.0f * t * t + t) * m0
-                    + (-2.0f * t * t * t + 3.0f * t * t) * p1
-                    + (t * t * t - t * t) * m1;
+                Vector3 tangent;
+                position = Interpolate(p0, p1, m0, m1, t, out tangent);
                 CurveCoordinates[i * CurveResolution + j] = position;
-
-                // Calculate tangents
-                // p'(t) = (6t² - 6t)p0 + (3t² - 4t + 1)m0 + (-6t² + 6t)p1 + (3t² - 2t)m1
-                Vector3 tangent = (6 * t * t - 6 * t) * p0
-                    + (3 * t * t - 4 * t + 1) * m0
-                    + (-6 * t * t + 6 * t) * p1
-                    + (3 * t * t - 2 * t) * m1;
                 Tangents[i * CurveResolution + j] = tangent;
                 //Debug.DrawRay(position, tangent.normalized * 2, Color.red);
                 Debug.DrawLine(position + Vector3.Cross(tangent, Vector3.up).normalized, position - Vector3.Cross(tangent, Vector3.up).normalized, Color.red);
