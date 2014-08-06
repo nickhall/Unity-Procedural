b954c99a8919428c66dca224de113373de539153
Nick Hall <iamssb@gmail.com>
2014-08-06 16:15:08 +0900

    Graph stuff

---
+++ Splines/Assets/Scripts/IGraph.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+interface IGraph<T>
+{
+    List<T> Nodes
+    {
+        get;
+        set;
+    }
+
+
+    void AddNode(T node);
+
+}
